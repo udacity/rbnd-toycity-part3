7da9e9dc8ff8249082e35d37b26d95cc313853ae
cendrars59 <cyr.leroux@outlook.com>
2016-02-09 15:52:04 +0100

    Add ability to create and count products

---
+++ lib/errors.rb
@@ -0,0 +1,3 @@
+class DuplicateProductError < StandardError
+
+end
--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,41 @@
 class Product
+
+  attr_accessor :title, :price, :stock
+  attr_reader :in_stock
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    @in_stock = is_in_stock?
+    add_to_products_list
   end
 
   def self.all
     @@products
+  end
+
+  def find_by_title(title)
+    @@products.find { |product| product.title == title }
+  end
+
+  def not_exist_in_list?
+    find_by_title(title)==nil
+  end
+
+  def is_in_stock?
+    @stock > 0
+  end
+
+
+  private
+
+  def add_to_products_list
+    if not_exist_in_list?
+      @@products << self
+    else
+      puts "already exist"
+    end
   end
 end
